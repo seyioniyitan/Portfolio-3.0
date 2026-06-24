b501327f45aa6e20792abe62e202d10f366bebe2
Ayomide Kolade <ayomikolade2003@gmail.com>
2026-06-24 15:28:42 +0100

    refactor: update Sanity configuration to use consistent environment variables for project ID and dataset, enhancing error handling for missing configurations

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_DATASET,
   apiVersion: "2025-01-01",
   useCdn: true,
 });
--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,11 +4,8 @@
  **/
 import { defineCliConfig } from "sanity/cli";
 
-const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
-  process.env.SANITY_STUDIO_PROJECT_ID;
-const dataset =
-  process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_STUDIO_DATASET;
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID;
+const dataset = process.env.SANITY_STUDIO_DATASET;
 
 if (!projectId || !dataset) {
   throw new Error("Missing Sanity configuration environment variables!");
